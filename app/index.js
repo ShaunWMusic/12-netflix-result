b9649a30f28486ac32fe6434b5db06b9ab61d659
“Shaun <shaunwmusic@gmail.com>
2016-10-21 09:47:20 -0500

    Finish JS setup, start NetflixResultView

--- app/index.js
+++ app/index.js
@@ -1,1 +1,9 @@
 import 'whatwg-fetch';
+
+
+// Create Variables for the search form, search input, and search result
+((data) => {
+  const f = document.querySelector('.btn');
+  const i = document.querySelector('.search');
+  const r = document.querySelector('.result');
+});
---
+++ views/netflix-result.js
@@ -0,0 +1,6 @@
+export default NetflixResultView(el, model) {
+  constructor(el, model) {
+    this.el = el;
+    this.model = model;
+  }
+}
