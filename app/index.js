7b3bc16b05ce5cbca4d47d690a6b5af6e225c673
“Shaun <shaunwmusic@gmail.com>
2016-10-21 16:04:56 -0500

    Update render and delete data.json

--- app/index.js
+++ app/index.js
@@ -1,10 +1,28 @@
 import 'whatwg-fetch';
-import data from './data.json';
 import NetflixResultView from './views/netflix-result';
 
 // Create Variables for the search form, search input, and search result
-// ((data) => {
-//   const f = document.querySelector('.btn');
-//   const i = document.querySelector('.search');
-//   const r = document.querySelector('.result');
-// });
+const searchBar = document.querySelector('.search-bar');
+const resultEl = document.querySelector('.result');
+
+const resultView = new NetflixResultView(resultEl, {});
+
+fetch('http://netflixroulette.net/api/api.php?title=Scrubs')
+  .then((res) => res.json())
+  .then((data) => {
+    resultView.model = data;
+    resultView.render();
+  });
+
+// debugger;
+// const view = new NetflixResultView(r, data);
+  // function afterData(shows) {
+  //   fillResultsList(r, shows);
+  // }
+
+  // function afterClick() {
+  //   data(i.value)
+  // .then(afterData);
+  // }
+
+// f.addEventListener('click', afterClick);
--- app/views/netflix-result.js
+++ app/views/netflix-result.js
@@ -1,27 +1,9 @@
-export default class NetflixResultView(data) {
-  constructor(model) {
+export default class NetflixResultView {
+  constructor(el, model) {
     this.el = el;
     this.model = model;
-
-    this.element = document.createElement('div');
-    this.element.classList.add('result');
-    this.element.innerHTML = `
-    <div class="result-content">
-      <div class="result-content__item">
-        <div class="result-content__item--text">
-          <h2 class="result-title"></h2>
-          <h5 class="result-category"></h5>
-          <h3 class="result-rating"><span class="fa fa-star"></span></h3>
-          </div>
-          <img src="" alt="" class="result-img"/>
-      </div>
-        <h2>Summary</h2>
-        <p class="result-summary"></p>
-        <h2>Actors</h2>
-        <p class="result-actors"></p>
-      </div>
-    `
   }
+
   render() {
     this.el.querySelector('.result-title').innerText = this.model.show_title;
     this.el.querySelector('.result-category').innerText = this.model.category;
@@ -30,4 +12,4 @@ export default class NetflixResultView(data) {
     this.el.querySelector('.result-summary').innerText = this.model.summary;
     this.el.querySelector('.result-actors').innerText = this.model.show_cast;
   }
-};
+}
